6e6ddbfd183a65681cd16440c49dabaf371e45f4
kunioukawa <kunioukawa@gmail.com>
2014-11-25 10:26:28 +0900

    Reviced201411125

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,51 @@
-angular.module('myApp', ['onsen.directives']);
-(function() {
-    var app = angular.module('myApp', ['onsen.directives', 'ngTouch']);
+$(function() {
+  
+  // 「スキャンする」を押したときのイベント
+  $("#ScanButton").click(function() {
+    scanBarcode();
+    return false;
+  });
+  
+  // 「ブラウザで開く」を押したときのイベント
+  $("#BrowserOpenButton").click(function() {
+    // invokeBrowserを用いてブラウザでURLを開く
+    monaca.invokeBrowser($("#ResultMessage").text());
+    monaca.pushPage('index.html');
+    return false;
+  });
 
-    document.addEventListener('deviceready', function() {
-        angular.bootstrap(document, ['myApp']);
-    }, false);
-
-    app.factory('Data', function() {
-        var Data = {};
-        Data.items = ['aa', 'bb', 'cc'];
-        return Data;
-    });
-
-    app.controller('Page1Ctrl', function($scope, Data) {
-        $scope.items = Data.items;
-
-        $scope.next = function(index) {
-            Data.index = index;
-            var item = Data.items[index];
-            $scope.ons.navigator.pushPage('page2.html', {title: item});
-        };
-    });
-
-    app.controller('Page2Ctrl', function($scope, Data) {
-        $scope.item = Data.items[Data.index];
-
-        $scope.save = function() {
-            Data.items[Data.index] = $scope.item;
-            $scope.ons.navigator.popPage();
-        };
-    });
-})();
+  $("#BackButton").click(function() {
+    // 最初の画面に戻る
+    monaca.pushPage('index.html');
+    return false;
+  });
+  
+});
+ 
+// 「スキャンする」を押したときに実行される関数
+function scanBarcode() {
+  // BarcodeScannerプラグインを利用してスキャン
+  window.plugins.barcodeScanner.scan(
+    // 成功時に実行されるコールバック（キャンセル時も含む）
+    function(result) {
+      // キャンセルされたら何もしない
+      if (result.cancelled) {
+        return;
+      }
+      
+      // 結果テキストを表示
+      $("#ResultMessage").text(result.text);
+      
+      // URLなら「ブラウザで開く」ボタンを表示
+      if (result.text.indexOf("http") === 0) {
+        $("#BrowserOpenButton").show();
+      } else {
+        $("#BrowserOpenButton").hide();
+      }
+    },
+    // エラー時に実行されるコールバック
+    function(error) {
+      $("#ResultMessage").text(error);
+    }
+  );
+}
